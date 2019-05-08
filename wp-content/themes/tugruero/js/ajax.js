8480d86e64c3ad42499e4e11785489d5c39e2f4e
Leonardo Hernandez <ing.leonardo.hp@gmail.com>
2019-05-08 09:34:27 -0400

    navegacion trampa

--- wp-content/themes/tugruero/js/ajax.js
+++ wp-content/themes/tugruero/js/ajax.js
@@ -10,7 +10,8 @@
                 quantity: $('input#cantidad').val()
             },
             success : function( response ) {
-                setTimeout(window.location="/finalizar-compra", 1000);	
+                setTimeout(window.location="/tugruero/finalizar-compra", 1000);	
+                // setTimeout(window.location="/finalizar-compra", 1000);	
             }
         });
     });
