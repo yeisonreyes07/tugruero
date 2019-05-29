7a19c54939709bf267a488fe064cc9fbcdf43f30
Leonardo Hernandez <ing.leonardo.hp@gmail.com>
2019-05-29 10:30:54 -0400

    db

---
+++ wp-content/plugins/woo-doc/woo-doc.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Created PDF of WOO
+ * Plugin URI: https://osba.com.ve
+ * Description: Crear archivos pdf de woo.
+ * Version: 0.3
+ * Author: Leonardo Hernandez
+ * Author URI: https://osba.com.ve
+ * Copyright 2019-2020. All rights reserved.
+ */
+?>
