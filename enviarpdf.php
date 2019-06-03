7aed07d8b781ee7d54ff00188bbb80ed4f715ee8
Leonardo Hernandez <ing.leonardo.hp@gmail.com>
2019-06-03 15:38:22 -0400

    gmail

--- enviarpdf.php
+++ enviarpdf.php
@@ -31,8 +31,8 @@
             $mail->SMTPAuth = true;                               // Enable SMTP authentication
             $mail->Username = "tugruerodesarrollo@gmail.com";                 // SMTP username
             $mail->Password = "perro.0000";                           // SMTP password
-            $mail->SMTPSecure = "tls";                            // Enable TLS encryption, `ssl` also accepted
-            $mail->Port = "587";                                    // TCP port to connect to
+            $mail->SMTPSecure = "ssl";                            // Enable TLS encryption, `ssl` also accepted
+            $mail->Port = "465";                                    // TCP port to connect to
         
             //Recipients
             $mail->setFrom("tugruerodesarrollo@gmail.com", "TuGruero");
