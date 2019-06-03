fd13a6f81e64882b682abb1722697b0348f3a644
Leonardo Hernandez <ing.leonardo.hp@gmail.com>
2019-06-03 13:13:38 -0400

    asd

--- generarpdf.php
+++ generarpdf.php
@@ -1,7 +1,8 @@
 <?php
+echo dirname(__FILE__).'./vendor/autoload.php';
     // require_once dirname(__FILE__).'./vendor/autoload.php';
 
-    session_start();
+    // session_start();
 
     // use Spipu\Html2Pdf\Html2Pdf;
     // use Spipu\Html2Pdf\Exception\Html2PdfException;
@@ -16,19 +17,18 @@
     //     ob_start();
     //     include "reshtmldoc/template.php";
     //     $content = ob_get_clean();
-    //     echo $content;
     //     // $temp = file_get_contents("reshtmldoc/template.php");
-    //     // try {
-    //     //     $html2pdf = new Html2Pdf('P', 'A4', 'fr',true, 'UTF-8', array(0, 0, 0, 0));
-    //     //     $html2pdf->pdf->SetDisplayMode('fullpage');
-    //     //     $html2pdf->setDefaultFont('Arial');
-    //     //     $html2pdf->writeHTML($content);
-    //     //     $html2pdf->output('example00.pdf');
-    //     // } catch (Html2PdfException $e) {
-    //     //     $html2pdf->clean();
-    //     //     $formatter = new ExceptionFormatter($e);
-    //     //     echo $formatter->getHtmlMessage();
-    //     // }
+    //     try {
+    //         $html2pdf = new Html2Pdf('P', 'A4', 'fr',true, 'UTF-8', array(0, 0, 0, 0));
+    //         $html2pdf->pdf->SetDisplayMode('fullpage');
+    //         $html2pdf->setDefaultFont('Arial');
+    //         $html2pdf->writeHTML($content);
+    //         $html2pdf->output('example00.pdf');
+    //     } catch (Html2PdfException $e) {
+    //         $html2pdf->clean();
+    //         $formatter = new ExceptionFormatter($e);
+    //         echo $formatter->getHtmlMessage();
+    //     }
     //     // $_SESSION['data_wc_lwc'] = "";
     // }else{
     //     header("location: ./");
