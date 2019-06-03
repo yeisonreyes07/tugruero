2f18ee7f3628c377332a99f1933a3728cc8842eb
Leonardo Hernandez <ing.leonardo.hp@gmail.com>
2019-06-03 14:08:23 -0400

    asd

--- reshtmldoc/template.php
+++ reshtmldoc/template.php
@@ -18,40 +18,5 @@
     table.bordertitulo{border-bottom: solid 1mm #222;}
 </style>
 <page backtop="28mm" backbottom="18mm" backleft="10mm" backright="10mm" style="font-size: 9pt">
-    <page_header>
-        <table class="page_header">
-            <tr>
-                <td style="width: 70%; text-align: left">
-                    <img src="http://67.205.188.242/tugruero/wp-content/themes/tugruero/images/logo.png" alt="" class="logo">
-                    <br>SOLUCIONES TU GRUERO C.A.<br>
-                    RIF: J-40680605-6
-                </td>
-                <td style="width: 40%; text-align: right">
-                    <table class="info">
-                        <tr>
-                            <td>Duración del Producto</td>
-                            <td>N° Producto</td>
-                        </tr>
-                        <tr>
-                            <td style="text-align: center">
-                                Anual
-                            </td>
-                            <td style="text-align: center">
-                                <?= $_SESSION['data_wc_lwc']->id; ?>
-                            </td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-        </table>
-    </page_header>
-    <page_footer>
-        <table class="page_footer">
-            <tr>
-                <td style="width: 100%; text-align: left;">
-                    Caracas, Venezuela · (212) 2379227 / 0500 GRUERO - 0 (0500 478376 - 0) ·  comercializacion@tugruero.io · www.tugruero.io
-                </td>
-            </tr>
-        </table>
-    </page_footer>
+    
 </page>
