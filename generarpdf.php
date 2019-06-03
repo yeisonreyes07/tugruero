2b3cda913509704e3500bec3451f47786fc04de0
Leonardo Hernandez <ing.leonardo.hp@gmail.com>
2019-06-03 13:06:11 -0400

    asd

--- generarpdf.php
+++ generarpdf.php
@@ -16,18 +16,19 @@
         ob_start();
         include "reshtmldoc/template.php";
         $content = ob_get_clean();
+        echo $content;
         // $temp = file_get_contents("reshtmldoc/template.php");
-        try {
-            $html2pdf = new Html2Pdf('P', 'A4', 'fr',true, 'UTF-8', array(0, 0, 0, 0));
-            $html2pdf->pdf->SetDisplayMode('fullpage');
-            $html2pdf->setDefaultFont('Arial');
-            $html2pdf->writeHTML($content);
-            $html2pdf->output('example00.pdf');
-        } catch (Html2PdfException $e) {
-            $html2pdf->clean();
-            $formatter = new ExceptionFormatter($e);
-            echo $formatter->getHtmlMessage();
-        }
+        // try {
+        //     $html2pdf = new Html2Pdf('P', 'A4', 'fr',true, 'UTF-8', array(0, 0, 0, 0));
+        //     $html2pdf->pdf->SetDisplayMode('fullpage');
+        //     $html2pdf->setDefaultFont('Arial');
+        //     $html2pdf->writeHTML($content);
+        //     $html2pdf->output('example00.pdf');
+        // } catch (Html2PdfException $e) {
+        //     $html2pdf->clean();
+        //     $formatter = new ExceptionFormatter($e);
+        //     echo $formatter->getHtmlMessage();
+        // }
         // $_SESSION['data_wc_lwc'] = "";
     }else{
         header("location: ./");
