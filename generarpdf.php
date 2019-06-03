11113813ee8ac7ab64b90cde75997d6b8afedecf
Leonardo Hernandez <ing.leonardo.hp@gmail.com>
2019-06-03 13:19:00 -0400

    asd

--- generarpdf.php
+++ generarpdf.php
@@ -10,25 +10,26 @@
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
 
-    var_dump($_SESSION['data_wc_lwc']);
+    // var_dump($_SESSION['data_wc_lwc']);
 
-    // if(!empty($_SESSION['data_wc_lwc'])){
-    //     ob_start();
-    //     include "reshtmldoc/template.php";
-    //     $content = ob_get_clean();
-    //     try {
-    //         $html2pdf = new Html2Pdf('P', 'A4', 'fr',true, 'UTF-8', array(0, 0, 0, 0));
-    //         $html2pdf->pdf->SetDisplayMode('fullpage');
-    //         $html2pdf->setDefaultFont('Arial');
-    //         $html2pdf->writeHTML($content);
-    //         $html2pdf->output('example00.pdf');
-    //     } catch (Html2PdfException $e) {
-    //         $html2pdf->clean();
-    //         $formatter = new ExceptionFormatter($e);
-    //         echo $formatter->getHtmlMessage();
-    //     }
-    //     // $_SESSION['data_wc_lwc'] = "";
-    // }else{
-    //     header("location: ./");
-    // }
+    if(!empty($_SESSION['data_wc_lwc'])){
+        ob_start();
+        include "reshtmldoc/template.php";
+        $content = ob_get_clean();
+        echo $content;
+        // try {
+        //     $html2pdf = new Html2Pdf('P', 'A4', 'fr',true, 'UTF-8', array(0, 0, 0, 0));
+        //     $html2pdf->pdf->SetDisplayMode('fullpage');
+        //     $html2pdf->setDefaultFont('Arial');
+        //     $html2pdf->writeHTML($content);
+        //     $html2pdf->output('example00.pdf');
+        // } catch (Html2PdfException $e) {
+        //     $html2pdf->clean();
+        //     $formatter = new ExceptionFormatter($e);
+        //     echo $formatter->getHtmlMessage();
+        // }
+        // $_SESSION['data_wc_lwc'] = "";
+    }else{
+        header("location: ./");
+    }
 ?>
