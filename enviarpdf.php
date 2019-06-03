b47f54746f87955b0b415dc6d7dee8f987ead15f
Leonardo Hernandez <ing.leonardo.hp@gmail.com>
2019-06-03 15:18:07 -0400

    asd

--- enviarpdf.php
+++ enviarpdf.php
@@ -22,8 +22,8 @@
             $html2pdf = new Html2Pdf('P', 'A4', 'fr',true, 'UTF-8', array(0, 0, 0, 0));
             $html2pdf->pdf->SetDisplayMode('fullpage');
             $html2pdf->setDefaultFont('Arial');
-            $html2pdf->writeHTML($content);
-            $html2pdf->output('reshtmldoc/cuadro'.$_SESSION['data_wc_lwc']->id.'.pdf',"F");
+            // $html2pdf->writeHTML($content);
+            // $html2pdf->output('reshtmldoc/cuadro'.$_SESSION['data_wc_lwc']->id.'.pdf',"F");
 
             $mail->isSMTP();                                      // Set mailer to use SMTP
             $mail->SMTPDebug = 2; 
--- vendor/tecnickcom/tcpdf/include/tcpdf_static.php
+++ vendor/tecnickcom/tcpdf/include/tcpdf_static.php
@@ -1818,6 +1818,7 @@ class TCPDF_STATIC {
 		} elseif (stream_is_local($filename) !== true) {
 			return false;
 		}
+		echo $_SERVER['DOCUMENT_ROOT']."tugruero/".$filename;
 		return fopen($_SERVER['DOCUMENT_ROOT']."tugruero/".$filename, $mode);
 	}
 
