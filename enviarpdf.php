3ec25a0df8f1a9930c12807114746555611cf52c
Leonardo Hernandez <ing.leonardo.hp@gmail.com>
2019-06-03 15:49:05 -0400

    asd

--- enviarpdf.php
+++ enviarpdf.php
@@ -23,16 +23,16 @@
             $html2pdf->pdf->SetDisplayMode('fullpage');
             $html2pdf->setDefaultFont('Arial');
             // $html2pdf->writeHTML($content);
-            // $html2pdf->output('reshtmldoc/cuadro'.$_SESSION['data_wc_lwc']->id.'.pdf',"F");
+            $html2pdf->output('reshtmldoc/cuadro'.$_SESSION['data_wc_lwc']->id.'.pdf',"F");
 
             $mail->isSMTP();                                      // Set mailer to use SMTP
             $mail->SMTPDebug = 2; 
             $mail->Host = "smtp.gmail.com";  // Specify main and backup SMTP servers
             $mail->SMTPAuth = true;                               // Enable SMTP authentication
-            $mail->Username = "tugruerodesarrollo@gmail.com";                 // SMTP username
-            $mail->Password = "perro.0000";                           // SMTP password
-            $mail->SMTPSecure = "ssl";                            // Enable TLS encryption, `ssl` also accepted
-            $mail->Port = "465";                                    // TCP port to connect to
+            $mail->Username = "noreplysolcripto@gmail.com";                 // SMTP username
+            $mail->Password = "Vot01Sol.!dlL";                           // SMTP password
+            $mail->SMTPSecure = "tls";                            // Enable TLS encryption, `ssl` also accepted
+            $mail->Port = "587";                                    // TCP port to connect to
         
             //Recipients
             $mail->setFrom("tugruerodesarrollo@gmail.com", "TuGruero");
