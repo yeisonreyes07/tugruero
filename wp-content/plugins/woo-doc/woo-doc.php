b49840a85742f6bfae1f2befecaddc65011b060f
Leonardo Hernandez <ing.leonardo.hp@gmail.com>
2019-07-11 10:10:44 -0400

    prueba 3

--- wp-content/plugins/woo-doc/woo-doc.php
+++ wp-content/plugins/woo-doc/woo-doc.php
@@ -22,11 +22,14 @@ if(!function_exists("lwc_get_opciones_de_admin")){
         if(isset($_GET['id'])){
             $query = new WC_Order($_GET['id']);
             $_SESSION['data_wc_lwc']=json_decode($query);
-            var_dump($_SESSION['data_wc_lwc']);
-            if(!$_SESSION['data_wc_lwc']->meta_data[16]->value){
-                $_SESSION['data_wc_lwc']->meta_data[15]->value = null; 
-                $_SESSION['data_wc_lwc']->meta_data[16]->value = null; 
+            if(!$_SESSION['data_wc_lwc']->meta_data[16]->key=='_billing_tipovental'){
+                $_SESSION['data_wc_lwc']->tipoventa = null; 
+                $_SESSION['data_wc_lwc']->canal = null; 
+            }else{
+                $_SESSION['data_wc_lwc']->tipoventa = $_SESSION['data_wc_lwc']->meta_data[16]->value; 
+                $_SESSION['data_wc_lwc']->canal = $_SESSION['data_wc_lwc']->meta_data[15]->value; 
             }
+            var_dump($_SESSION['data_wc_lwc']);
             $items = $query->get_items();
             foreach ( $items as $item ) {
                 $product_name = $item['name'];
