8bc8e9a277b90da7a5ddcf8fac050b36c2dd1e04
Leonardo Hernandez <ing.leonardo.hp@gmail.com>
2019-07-31 16:27:40 -0400

    actualizacion

--- wp-content/themes/tugruero/header.php
+++ wp-content/themes/tugruero/header.php
@@ -19,7 +19,7 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/js/slick/slick.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/js/slick/slick-theme.css">
-	
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 	<?php wp_head(); ?>
 </head>
 
--- wp-content/themes/tugruero/template-parts/content-faq.php
+++ wp-content/themes/tugruero/template-parts/content-faq.php
@@ -9,7 +9,6 @@
  */
 get_header();
 ?>
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post();?>	
     <h1 class="title">Preguntas frecuentes</h1>
     <div class="content-faqs">
