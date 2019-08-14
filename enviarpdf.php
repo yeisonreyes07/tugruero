cc03ba33cdf5287b73555c8da45b33caca1cfb59
Leonardo Hernandez <ing.leonardo.hp@gmail.com>
2019-08-14 14:34:17 -0400

    prueba email

--- enviarpdf.php
+++ enviarpdf.php
@@ -7,27 +7,23 @@
     use Spipu\Html2Pdf\Exception\Html2PdfException;
     use Spipu\Html2Pdf\Exception\ExceptionFormatter;
 
-    // var_dump($_SESSION['data_wc_lwc']);
-
     if(!empty($_SESSION['data_wc_lwc'])){
         ob_start();
         include "reshtmldoc/template.php";
         $content = ob_get_clean();
-        // echo $content;
-        $html2pdf = new Html2Pdf('P', 'A4', 'fr',true, 'UTF-8', array(0, 0, 0, 0));
-        $html2pdf->pdf->SetDisplayMode('fullpage');
-        $html2pdf->setDefaultFont('Arial');
-        $html2pdf->writeHTML("leo");
-        $html2pdf->output('reshtmldoc/cuadro'.$_SESSION['data_wc_lwc']->id.'.pdf',"F");
+        try {
+            $html2pdf = new Html2Pdf('P', 'A4', 'fr',true, 'UTF-8', array(0, 0, 0, 0));
+            $html2pdf->pdf->SetDisplayMode('fullpage');
+            $html2pdf->setDefaultFont('Arial');
+            $html2pdf->writeHTML($content);
+            $html2pdf->output('reshtmldoc/cuadro'.$_SESSION['data_wc_lwc']->id.'.pdf',"F");
 
-        header("location: enviar.php");
-        // try {
-        // } catch (Html2PdfException $e) {
-        //     $html2pdf->clean();
-        //     $formatter = new ExceptionFormatter($e);
-        //     echo $formatter->getHtmlMessage();
-        // }
-        // $_SESSION['data_wc_lwc'] = "";
+            header("location: enviar.php");
+        } catch (Html2PdfException $e) {
+            $html2pdf->clean();
+            $formatter = new ExceptionFormatter($e);
+            echo $formatter->getHtmlMessage();
+        }
     }else{
         header("location: ./");
     }
--- wp-content/themes/tugruero/template-parts/content-plan.php
+++ wp-content/themes/tugruero/template-parts/content-plan.php
@@ -107,7 +107,6 @@ get_header();
 		<div class="modal-dialog modal-lg" role="document">
 			<div class="modal-content">
 			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 				<h4 class="modal-title" id="myModalLabel">¡INFORMACION IMPORTANTE!</h4>
 			</div>
 			<div class="modal-body">
