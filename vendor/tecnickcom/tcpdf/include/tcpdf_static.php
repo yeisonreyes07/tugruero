33da542df48551a022741960fc8d16c6750f745d
Leonardo Hernandez <ing.leonardo.hp@gmail.com>
2019-08-14 15:41:33 -0400

    prueba email

--- vendor/tecnickcom/tcpdf/include/tcpdf_static.php
+++ vendor/tecnickcom/tcpdf/include/tcpdf_static.php
@@ -1818,7 +1818,7 @@ class TCPDF_STATIC {
 		} elseif (stream_is_local($filename) !== true) {
 			return false;
 		}
-		return fopen($_SERVER['DOCUMENT_ROOT']."tugruero/".$filename, $mode);
+		return fopen($_SERVER['DOCUMENT_ROOT']."/".$filename, $mode);
 	}
 
 	/**
